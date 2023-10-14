abecfbd7224362e2bfe9eb31431703356768f32f
HafizNRN <hfznurr12@gmail.com>
2023-10-14 08:15:52 +0700

    first commit

---
+++ weatherAPI.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+let search = document.getElementById("search-location");
+search.addEventListener("keydown", (event) => {
+  event.preventDefault();
+
+  let icon = document.querySelector("#weather-icon img");
+  let temp = document.getElementById("temp-data");
+  let country = document.getElementById("country-data");
+  let humidity = document.getElementById("humidity-data");
+  let wind = document.getElementById("wind-data");
+
+  if (event.key === "Enter") {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&appid=${process.env.API_KEY}&units=metric`
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        icon.src = `https://openweathermap.org/img/wn/${response.weather[0].icon}.png`;
+        temp.innerHTML = `${response.main.temp}`;
+        country.innerHTML = `${response.sys.country}`;
+        humidity.innerHTML = `${response.main.humidity}`;
+        wind.innerHTML = `${response.wind.speed}`;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    search.value = "";
+  }
+});
